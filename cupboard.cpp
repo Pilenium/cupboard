00080228729d6301aad50ca593a57e35c7b9829e
Pilenium <fiesvamp@gmail.com>
2017-05-15 00:25:38 +0300

    v1.1

--- cupboard.cpp
+++ cupboard.cpp
@@ -20,6 +20,7 @@ void create(){
 		cout << "goodbye" << endl;
 		exit(0);
 	}
+cout << endl;
 }
 Cupboard(){
 	cout << "Do you want to create your own cupboard? |Yes or no (y,n)|" << endl;
@@ -28,70 +29,78 @@ Cupboard(int sizel, int sizew, int sizeh){
 	this->sizel = sizel;
 	this->sizew = sizew;
 	this->sizeh = sizeh;
-	cout << "Example sizes of cupboard: " << endl;
-	cout << "Lenght = " << sizel << endl;
-	cout << "Width = " << sizew << endl;
-	cout << "Height = " << sizeh << endl;
+	cout << "__________________________" << endl << "Sizes of example cupboard: " << endl;
+	cout << "|Lenght = " << sizel << "|" << endl;
+	cout << "|Width = " << sizew << "  |" << endl;
+	cout << "|Height = " << sizeh << "|" << endl << "__________________________" << endl;
 }
 void setCupboardsizes(){
 	cout << "Enter sizes of your cupboard:" << endl;
 	cout << "Enter length: "; cin >> sizel;
 	cout << "Enter width: ";  cin >> sizew;
 	cout << "Enter height: "; cin >> sizeh;
-}
-void getCupboardsizes(){
-	cout << "Sizes of your cupboard" << endl << "Length: " << sizel << endl << "Width: "<< sizew << endl << "Height: " << sizeh << endl;
+	space = sizel*sizew*sizeh;
 }
 void clothes(){
 int amountclothes = 0;
 bclothes = false;
 int sw = sizew;
-	while ((sizel > 44) && (sw > 1) && (sizeh > 89)){
+int clsizelmax = 44, clsizewmax = 1, clsizehmax = 89;
+	while ((sizel > clsizelmax) && (sw > clsizewmax) && (sizeh > clsizehmax)){
 		amountclothes = amountclothes + 1;
 		sw = sw -2;
 		bclothes = true;
 	}
 	if (bclothes == false){
-		cout << " You can't put clothes on the hanger in your cupboard " << endl;
+		cout << endl << " You can't put clothes on the hanger in your cupboard " << endl;
 	}
 	else if (bclothes == true){
-		cout << "Amount of clothes on the hanger in your cupboard: " << amountclothes << endl;
+		cout << endl <<"Amount of clothes on the hanger in your cupboard: " << amountclothes << endl;
+		
 	}
 }
+float space;
 void changevolume(){
-	float svol1, svol2, svol3, svol4;
-	svol1 = sizel*sizew*sizeh;
-	svol2 = 45*sizew*90;
-	svol3 = 36*45*52;
+	float cvoluser, cvolclothes, cvolsafe, cvolcompare;
+	cvoluser = sizel*sizew*sizeh;
+	cvolclothes = 45*sizew*90;
+	cvolsafe = 36*45*52;
 	if (bclothes == true){
-		svol4 = 100-((svol1-svol2)/svol1*100);
-		cout << "your cupboard will be smaller on " << svol4 << "% if you have only clothes" << endl;
+		cvolcompare = 100-((cvoluser-cvolclothes)/cvoluser*100);
+		cout << endl << "Your cupboard will be smaller on " << cvolcompare << "% if you have only clothes" << endl;
+		space = cvoluser-cvolclothes;
 } 
 	if (bsafe == true){
-		svol4 = 100-((svol1-svol3)/svol1*100);
-		cout << "your cupboard will be smaller on " << svol4 << "% if you have only safe" << endl;
+		cvolcompare = 100-((cvoluser-cvolsafe)/cvoluser*100);
+		cout << endl << "Your cupboard will be smaller on " << cvolcompare << "% if you have only safe" << endl;
+		space = cvoluser-cvolsafe;
 }
 	if ((bsafe == true)&&(bclothes == true)){
-		svol4 = 100-((svol1-svol2-svol3)/svol1*100);
-		cout << "your cupboard will be smaller on " << svol4 << "% if you have safe and clothes" << endl;
+		cvolcompare = 100-((cvoluser-cvolclothes-cvolsafe)/cvoluser*100);
+		cout << endl << "Your cupboard will be smaller on " << cvolcompare << "% if you have safe and clothes" << endl;
+		space = cvoluser-cvolclothes-cvolsafe;
 }
 }
 void volume(){
-	float vol1, vol2, vol3;
-	vol1 = sizel*sizew*sizeh;
-	vol2 = 200*70*200;
-	if(vol2 < vol1){
-		vol3 = 100-((vol2/vol1)*100);
-		cout << "standard cupboard is smaller than yours on " << vol3 << " %" << endl;
+	float voluser, volexample, volcompare;
+	voluser = sizel*sizew*sizeh;
+	volexample = 200*70*200;
+	if(volexample < voluser){
+		volcompare = 100-((volexample/voluser)*100);
+		cout << endl << "Example cupboard is smaller than yours on " << volcompare << " %" << endl;
 }
-	else if (vol1 < vol2){
-		vol3 = 100-((vol1/vol2)*100);
-		cout << "Standard cupboard is bigger than yours on " << vol3 << " %" << endl;
+	else if (voluser < volexample){
+		volcompare = 100-((voluser/volexample)*100);
+		cout << endl << "Example cupboard is bigger than yours on " << volcompare << " %" << endl;
 }
-	else if (vol1 == vol2) cout << "Volumes are equal" << endl;
+	else if (voluser == volexample) cout << endl << "Volumes are equal" << endl;
+}
+void getCupboardvolume(){
+	cout << endl << "Volume of your cupboard in cubic meters: " << (space/1000000) << endl;
+	
 }
 void csafe(){
-	cout << "Do you have safe in your cupboard? |Yes or no(y,n)|" << endl;
+	cout << endl << "Do you want to shove a safe in the cupboard? |Yes or no(y,n)|" << endl;
 	bsafe = false;
 	string ans;
 	cin >> ans;
@@ -101,11 +110,12 @@ void csafe(){
 }
 	if (ans == "y"){
 		if ((sizel > 36) && (sizew > 45) && (sizeh > 52)) bsafe = true;
+			else cout << endl << "You can't put safe in your cupboard" << endl;
 	}
 }
 ~Cupboard(){
 	amountcup++;
-	cout << "Destruct, there was " << amountcup << " cupboard" << endl << endl;
+	cout << endl << "Destruct, there was " << amountcup << " cupboard" << endl << endl;
 }
 };
 int Cupboard::amountcup;
@@ -114,12 +124,11 @@ int main(int argc, char* argv[]){
 	Cupboard obj1;
 	obj1.create();
 	obj1.setCupboardsizes();
+	obj1.getCupboardvolume();
 	obj1.volume();
 	obj1.clothes();
 	obj1.csafe();
 	obj1.changevolume();
-	obj1.getCupboardsizes();
+	obj1.getCupboardvolume();
 	return 0;
 }
-// 45 - lenght, 90 height, 5 width()clothes
-// 200 - height, 200 - lenght, width - 70() cupboard
